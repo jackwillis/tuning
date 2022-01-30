54b0f459536b2927909f55851373c353a52a9916
Jack Willis <jack@attac.us>
2022-01-30 01:50:30 -0600

    change scale to tuning

--- src/main.cpp
+++ src/main.cpp
@@ -2,58 +2,174 @@
 #include <iostream>
 #include <variant>
 #include <vector>
+
 #include <boost/rational.hpp>
 
-struct CentsVisitor
-{
-  const int CENTS_PER_OCTAVE = 1200;
+// Refer to source of haskell package hmt
+// https://hackage.haskell.org/package/hmt-0.16/docs/
 
+const int CENTS_PER_OCTAVE = 1200;
+
+// TuningInterval#cents(double)
+// TuningInterval#cents(rational)
+struct TuningIntervalCentsVisitor
+{
   double operator()(double cents) const
   {
     return cents;
   }
 
+  // convert ratio to cents
   double operator()(boost::rational<int> ratio) const
   {
-    double f_ratio = boost::rational_cast<double>(ratio);
+    const double f_ratio = boost::rational_cast<double>(ratio);
     return std::log2(f_ratio) * CENTS_PER_OCTAVE;
   }
 };
 
-class ScaleInterval
+// TuningInterval#ratio(double)
+// TuningInterval#ratio(rational)
+struct TuningIntervalRatioVisitor
+{
+  // convert cents to ratio
+  double operator()(double cents) const
+  {
+    return std::pow(2, cents / CENTS_PER_OCTAVE);
+  }
+
+  double operator()(boost::rational<int> ratio) const
+  {
+    return boost::rational_cast<double>(ratio);
+  }
+};
+
+// TuningInterval#str(double)
+// TuningInterval#str(rational)
+struct TuningIntervalStrVisitor
+{
+  std::string operator()(double cents) const
+  {
+    return std::to_string(cents);
+  }
+
+  std::string operator()(boost::rational<int> ratio) const
+  {
+    std::stringstream rational_ss;
+    rational_ss << ratio;
+    return rational_ss.str();
+  }
+};
+
+// Tuning intervals can be written as either
+// cents (type double), meaning percent of a semitone, or as a
+// ratio (type rational).
+// This class is a wrapper for a std::variant of the two,
+// which depends on some visitor structs,
+// particularly for converting between cents and ratios.
+//
+class TuningInterval
 {
 private:
   const std::variant<double, boost::rational<int>> v;
 
 public:
-  ScaleInterval(double cents) : v(cents){};
-  ScaleInterval(int numer, int denom) : v(boost::rational<int>(numer, denom)){};
+  TuningInterval(double cents) : v(cents){};
+  TuningInterval(int numer, int denom) : v(boost::rational<int>(numer, denom)){};
 
-  double cents()
+  double cents() const
   {
-    return std::visit(CentsVisitor{}, v);
+    return std::visit(TuningIntervalCentsVisitor{}, this->v);
+  }
+
+  double ratio() const
+  {
+    return std::visit(TuningIntervalRatioVisitor{}, this->v);
+  }
+
+  std::string str() const
+  {
+    return std::visit(TuningIntervalStrVisitor{}, this->v);
+  }
+};
+
+const int column_width = 15; // max length of double
+auto pad = std::setw(column_width);
+
+class Tuning
+{
+private:
+  const std::string name;
+  const std::string description;
+  const std::vector<TuningInterval> intervals;
+
+public:
+  Tuning(std::string name,
+         std::string description,
+         std::vector<TuningInterval> intervals)
+      : name(name), description(description), intervals(intervals){};
+
+  int degree() const
+  {
+    return (int)intervals.size();
+  }
+
+  void print_scala() const
+  {
+    const int column_width = 15; // max length of double
+    auto pad = std::setw(column_width);
+
+    std::cout << "! "
+              << this->name << "\n! \n"
+              << this->description << '\n'
+              << this->degree() << "\n!\n";
+
+    for (const auto itv : this->intervals)
+    {
+      std::cout << itv.str() << '\n';
+    }
+  }
+
+  void print_table() const
+  {
+    const int column_width = 15; // max length of double
+    auto pad = std::setw(column_width);
+
+    std::cout << "Value" << pad
+              << "Ratio" << pad
+              << '\n';
+
+    for (const auto itv : this->intervals)
+    {
+      std::cout
+          << pad << itv.str()
+          << pad << itv.ratio()
+          << '\n';
+    }
   }
 };
 
 int main()
 {
-  std::vector<ScaleInterval> bremmer_ebvt3 = {
-      ScaleInterval{94.87252},
-      ScaleInterval{197.05899},
-      ScaleInterval{297.80000},
-      ScaleInterval{395.79561},
-      ScaleInterval{4, 3},
-      ScaleInterval{595.89736},
-      ScaleInterval{699.31190},
-      ScaleInterval{796.82704},
-      ScaleInterval{896.20299},
-      ScaleInterval{999.10000},
-      ScaleInterval{1096.17389},
-      ScaleInterval{2, 1}};
+  const Tuning bremmer_ebvt3 = Tuning{
+      "bremmer_ebvt3.scl",
+      "Bill Bremmer EBVT III temperament (2011)",
+      std::vector<TuningInterval>{
+          TuningInterval{94.87252},
+          TuningInterval{197.05899},
+          TuningInterval{297.80000},
+          TuningInterval{395.79561},
+          TuningInterval{4, 3},
+          TuningInterval{595.89736},
+          TuningInterval{699.31190},
+          TuningInterval{796.82704},
+          TuningInterval{896.20299},
+          TuningInterval{999.10000},
+          TuningInterval{1096.17389},
+          TuningInterval{2, 1}}};
 
-  for (auto itv : bremmer_ebvt3)
-  {
-    std::cout << itv.cents() << '\n';
-  }
+  bremmer_ebvt3.print_scala();
+  std::cout << "---------------------------------------------\n";
+  bremmer_ebvt3.print_table();
+
   return EXIT_SUCCESS;
 }
